fd2457a6a43e6d0eadd4a7f55418a24791c6a217
Todor Gospodinov <gospodinov.web@gmail.com>
2015-09-26 18:51:52 +0300

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 //maps api key
 //
 
+var map;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
@@ -9,6 +11,6 @@ function initMap() {
 }
 
 $(document).ready(function(){
-    var map;
+
 
 });
